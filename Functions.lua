64cb3d7ceae3c512f3d704d6bef6b2b29372b97c
lox056594-dev <lox056594@gmail.com>
2026-05-27 15:45:06 +0300

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -1,1 +1,13 @@
+local Functions = {}
 
+function Functions:Fly(state)
+    print("Fly is", state)
+    -- Сюда вставляешь логику полета
+end
+
+function Functions:ESP(state)
+    print("ESP is", state)
+    -- Сюда логику ESP
+end
+
+return Functions
