74abdf2881791a183a698e7f999a08b0b88ac012
lox056594-dev <lox056594@gmail.com>
2026-05-27 15:44:48 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,7 @@
+local UI = require(script.Parent.UI)
+local Functions = require(script.Parent.Functions)
 
+local Window = UI:CreateWindow("Vanguard")
+
+UI:AddToggle(Window, "Fly", function(v) Functions:Fly(v) end)
+UI:AddToggle(Window, "ESP", function(v) Functions:ESP(v) end)
