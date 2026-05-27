2a485b52b999de9bd95e34e7358fca65d92ae282
lox056594-dev <lox056594@gmail.com>
2026-05-27 15:45:37 +0300

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -1,1 +1,7 @@
-
+return {
+    Bg = Color3.fromRGB(12, 11, 22),
+    Card = Color3.fromRGB(18, 17, 30),
+    Accent = Color3.fromRGB(160, 110, 255),
+    Neon = Color3.fromRGB(190, 140, 255),
+    Text = Color3.fromRGB(245, 240, 255)
+}
